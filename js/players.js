37c98d82dee4b6442f0ef0c7238d0df2ffe83caa
jmonloop <monleonlopezjavier@gmail.com>
2022-01-31 22:30:47 +0100

    reload method added to guns

--- js/players.js
+++ js/players.js
@@ -3,65 +3,76 @@
 
 //Declaration of Gun
 class Gun {
-    constructor(gunType, attack, ammo, fireRate) {
+    constructor(gunType, damage, attack, ammo, fireRate) {
+        //gunType-> Just name of the gun
         this.gunType = gunType;
+        //damage-> points of life will rest to the other player (min0-max100)
+        this.damage = damage;
+        //attack-> if gun is shooting (bool)
         this.attack = attack;
-        this.ammo = 1000;
-        // this.ammo = ammo;
+        //ammo-> quantity of attacks remaining before reload (min0-max150)
+        this.ammo = ammo;
+        this.initialAmmo = ammo;
+        //speed of attack and loss of ammo (min500 - max50)
         this.fireRate = fireRate;
     }
-
+    
     shoot(){
-
-        //Shoot method detects when user holds down left click button or touches screen
+        //shoot method detects when user holds down left click button or touches screen
         let holding;
-        let shots = 0;
-        let shots2;
 
+        //myTime determines the firing rate of the gun and so changes its variable properties
         const myTime = () => {
             let setTimeId = setTimeout(() =>{
-                shots +=1;
-                console.log(holding);
+                if(holding) {
+                    this.ammo -=1;
+                    this.attack = true;
+                    this.damage +=1;
+                    console.log("shooting", gun1);
+                    myTime();
 
 
-                if(holding === true) {
-                myTime();
-                } else clearTimeout(setTimeId);
+                }   else clearTimeout(setTimeId);
 
-            }, 1);
+        
+                //Minimum fireRate is 500, maximum is 50
+            }, this.fireRate);
         }
 
         window.addEventListener('mouseup', ()=>{
             holding = false;
-            // console.log('stop');
             myTime();
         })
         window.addEventListener('mousedown', (e)=>{
             if(e.button === 0) {
                 holding = true;
-                // console.log('shoot');
                 myTime();
             }
         })
         
 
     }
+
+    reload(){
+        window.addEventListener('mouseup', ()=>{
+            this.ammo = this.initialAmmo;
+            console.log("reload", gun1);
+        })
+    }
 }
-
-//Detects if left button is clicked and calls gun.shoot()
-
-
-
-
 
 
 
 //Instances guns
-let gun1 = new Gun("Glock", 30, 15, 2);
+let gun1 = new Gun("Pistol", 20, false, 15, 500);
+let gun2 = new Gun("MP5", 10, false, 25, 100);
+let gun3 = new Gun("Shotgun", 80, false, 8, 200);
+let gun4 = new Gun("AR15", 50, false, 30, 80);
 
 
 
-document.querySelector('#screen1').addEventListener('onmouseup', gun1.shoot());
+
+document.querySelector('#screen1').addEventListener('mouseup', gun1.shoot(), gun1.reload());
 
 
 
