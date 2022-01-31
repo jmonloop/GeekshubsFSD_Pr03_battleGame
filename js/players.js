616671f058fa416f90f915a899cd87f68f123e39
jmonloop <monleonlopezjavier@gmail.com>
2022-01-31 23:17:59 +0100

    characters added

--- js/players.js
+++ js/players.js
@@ -1,4 +1,4 @@
-// const types = [heavy, runner, sniper];
+
 
 
 //Declaration of Gun
@@ -29,7 +29,8 @@ class Gun {
                     if(this.ammo > 0) {
                         this.ammo -=1;
                         this.attack = true;
-                        console.log("shooting", gun2);
+                        console.log("player1 is shooting");
+                        player2.beDamaged();
                         myTime();
                     }
 
@@ -59,7 +60,6 @@ class Gun {
     reload(){
         window.addEventListener('mouseup', ()=>{
             this.ammo = this.initialAmmo;
-            console.log("reload", gun2);
         })
     }
 }
@@ -75,7 +75,7 @@ let gun4 = new Gun("AR15", 50, false, 30, 80);
 
 
 
-document.querySelector('#screen1').addEventListener('mouseup', gun2.shoot(), gun2.reload());
+
 
 
 
@@ -83,32 +83,44 @@ document.querySelector('#screen1').addEventListener('mouseup', gun2.shoot(), gun
 //Declaration of Character
 class Character {
 
-    constructor(charType, life, speed, stamina, gun) {
-        this.charType = charType;
+    constructor(characterType, life, gun, stamina, position) {
+        //Name of the differente characters
+        this.characterType = characterType;
+        //Life goes down when character is damaged (min0-max1000)
         this.life = life;
-        this.speed = speed;
-        this.stamina = stamina;
+        //gun is inherited from the object gun
         this.gun = gun;
+        //stamina determines how quickly the character gets out of cover (min0-max100)
+        this.stamina = stamina;
+        //position defines which position is the character when gets out of cover
+        this.position = position;
     }
 
 
     attack() {
-        return this.attack += 30;
+        this.gun.shoot();
+    }
+    cover() {
+        this.gun.reload();
     }
 
     beDamaged() {
-        return this.life -= 0;
+        if(player1.gun.attack === true) {
+            this.life -= player1.gun.damage;
+            console.log(player1, player2)
+        }
     }
 
 };
+let player1Pos = "derecha";
 
-// //Instanciando corredores...
+let player1 = new Character("Navy Seal", 1000, gun1, 100, player1Pos);
+let player2 = new Character("Spetnaz", 1000, gun1, 100, player1Pos);
+let player3 = new Character("Legionario", 1000, gun1, 100, player1Pos);
+let player4 = new Character("Ninja", 1000, gun1, 100, player1Pos);
 
-// let coche1 = new Coche("Gt","Ford",250,2000,50,"Gasolina")
-// let coche2 = new Coche("Celica","Toyota",251,1900,70,"Gasolina");
-// let coche3 = new Coche("TestaRossa","Ferrari",270,2300,55,"Gasolina");
-// let coche4 = new Coche("Camaro","Chevrolet",299,1750,90,"Diesel");
 
+document.querySelector('#screen1').addEventListener('mouseup', player1.attack(), player1.gun.reload());
 
 // //Generando variables básicas de entorno
 
