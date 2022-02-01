cbcfdc08318990c64863f3a238847ef47d846ed1
jmonloop <monleonlopezjavier@gmail.com>
2022-02-01 19:42:27 +0100

    hide method and animation added

--- js/players.js
+++ js/players.js
@@ -62,7 +62,7 @@ const ar15 = {
 //Declaration of Character
 class Character {
 
-    constructor(characterType, life, gun, stamina, position) {
+    constructor(characterType, life, gun, stamina, position, src) {
         //Name of the differente characters
         this.characterType = characterType;
         //Life goes down when character is damaged (min0-max1000)
@@ -73,8 +73,42 @@ class Character {
         this.stamina = stamina;
         //position defines which position is the character when gets out of cover
         this.position = position;
+        //src sets the png image for animating the character
+        this.src = src;
+
+
+        this.attack = false;
+        this.covered = true;
     }
 
+    show(){
+        if(this.covered === true) {
+            let player1Img = document.getElementById('_character1img');
+            let player1ImgSrc = player1Img.src;
+            player1ImgSrc = player1ImgSrc.replace("crouch", "shoot");
+            player1Img.src = player1ImgSrc;
+
+            this.covered = false;
+        }
+    }
+
+    //Hides and reloads ammo when click on the player1 wall
+    hide(){
+
+        if(this.covered === false) {
+        let player1Img = document.getElementById('_character1img');
+        let player1ImgSrc = player1Img.src;
+        player1ImgSrc = player1ImgSrc.replace("shoot", "crouch");
+        player1Img.src = player1ImgSrc;
+        this.gun.ammo = this.gun.initialAmmo;
+        this.covered = true;
+        }
+        
+        
+
+
+
+    }
 
     shoot(){
         //shoot method detects when user holds down left click button or touches screen
@@ -122,12 +156,7 @@ class Character {
 
     }
 
-    //Reload hides and fills ammo
-    reload(){
-        window.addEventListener('mouseup', ()=>{
-            this.gun.ammo = this.gun.initialAmmo;
-        })
-    }
+
 
     beDamaged() {
         if(player1.attack === true) {
@@ -143,12 +172,24 @@ class Character {
 };
 let player1Pos = "derecha";
 
-let player1 = new Character("Navy Seal", 1000, pistol, 100, player1Pos);
-let player2 = new Character("Spetnaz", 1000, mp5, 100, player1Pos);
-let player3 = new Character("Legionario", 1000, shotgun, 100, player1Pos);
-let player4 = new Character("Ninja", 1000, ar15, 100, player1Pos);
+let hero1src = './img/png/characters/crouch1.png'
+let hero2src = './img/png/characters/crouch2.png'
+let hero3src = './img/png/characters/crouch3.png'
+let hero4src = './img/png/characters/crouch4.png'
+
+
+let player1 = new Character("Navy Seal", 1000, pistol, 100, player1Pos, hero1src);
+let player2 = new Character("Spetnaz", 1000, mp5, 100, player1Pos, hero2src);
+let player3 = new Character("Legionario", 1000, shotgun, 100, player1Pos, hero3src);
+let player4 = new Character("Special Ops", 1000, ar15, 100, player1Pos, hero4src);
 
 
 
-// document.querySelector('#screen1').addEventListener('mouseup', player1.shoot());
+document.querySelector('_bottomWall').addEventListener('mousedown', player1.hide());
 // document.querySelector('#screen1').addEventListener('mouseup', player1.shoot(), player1.reload());
+
+
+
+
+
+
