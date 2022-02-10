8b88aeccb0a0b2fffa714587860f50a7c1998b17
jmonloop <monleonlopezjavier@gmail.com>
2022-02-10 21:35:23 +0100

    readme update

--- README.md
+++ README.md
@@ -6,89 +6,129 @@ The minimum requeriments are to have 3 different screens (start, select players 
 In this case I have opted for a shooting game controlled by the user and an autonomous AI enemy with different levels of difficulty.
 
 
+    
+[Features](#features)
 
+[Upcoming](#upcoming)
 
-![ScreenShot](https://raw.githubusercontent.com/jmonloop/GeekshubsFSD_Ch1_consoleGame/master/img/cap1.JPG)
+[Technologies](#technologies)
 
+[Sources Credits](#sources-credits)
 
+[Thanks](#thanks)
 
 * You can try web version here
-https://jmonloop.github.io/GeekshubsFSD_Ch1_consoleGame/
+https://jmonloop.github.io/GeekshubsFSD_Pr03_battleGame/
 
 
 
-## Features 📋
-* Point and click action game: the player only can shoot when he is unhidden but is also where he can be damaged.
+### Features 📋
+* Point and click action game:
+    * Players only can shoot when they are unhidden but is also when they can be damaged.
+    * Players only can move when they are covered.
+    * Player2 (AI) accuracy increases if Player1 remains static.
+
+![ScreenShot](https://raw.githubusercontent.com/jmonloop/GeekshubsFSD_Pr03_battleGame/master/assets/screenshots/battle.jpg)
+
+
+
 * Retro style with animated sprites that react to the mouse movement or player's position in case of the AI.
+
+![ScreenShot](https://raw.githubusercontent.com/jmonloop/GeekshubsFSD_Ch1_consoleGame/master/screenshots/running.jpg)
+
+
+
 * 3 options for choose character (fourth is locked at the beginning).
     Each of them with their own health points.
 * 4 options for choose weapon.
     Each of them with their own of ammo and damage parameters.
-* Random power-ups that improve weapon damage, increase health and even an xVision googles that allow players to damage the enemy still if he is hidden.
-* Drag and drop system to take the power-ups or throw the grenades.
 * 3 different difficulty levels with their own parameters agains the user (fourth is locked at the beginning). The AI behaviour also affects the way the enemy "steals" the spawned power-ups.
 * An adittional 4th character and difficulty level achieved only when the user has defeated the first 3 characters.
+
+![ScreenShot](https://raw.githubusercontent.com/jmonloop/GeekshubsFSD_Ch1_consoleGame/master/screenshots/selectplayers.jpg)
+
+
+
+* Random power-ups that improve weapon damage, increase health and even an xVision googles that allow players to damage the enemy still if he is hidden.
+* Drag and drop system to take the power-ups both mouse and touch system.
+
+![ScreenShot](https://raw.githubusercontent.com/jmonloop/GeekshubsFSD_Ch1_consoleGame/master/screenshots/powerup.jpg)
+
+
+
+
 * Statistics collection when the battle ends.
 
-* A cheat mode to win enemies with a secret code (more info [here](#secret-code))
+![ScreenShot](https://raw.githubusercontent.com/jmonloop/GeekshubsFSD_Ch1_consoleGame/master/screenshots/statistics.jpg)
 
 
 
-![ScreenShot](https://raw.githubusercontent.com/jmonloop/GeekshubsFSD_Ch1_consoleGame/master/img/dissasembled.JPG)
+* A secret cheat mode to win enemies with a secret code (more info [here](#secret-code))
+
+![ScreenShot](https://raw.githubusercontent.com/jmonloop/GeekshubsFSD_Ch1_consoleGame/master/screenshots/cheat.jpg)
 
 
 
-![ScreenShot](https://raw.githubusercontent.com/jmonloop/GeekshubsFSD_Ch1_consoleGame/master/img/start.JPG)
-
-
-![ScreenShot](https://raw.githubusercontent.com/jmonloop/GeekshubsFSD_Ch1_consoleGame/master/img/playing.JPG)
-
-
-
-![ScreenShot](https://raw.githubusercontent.com/jmonloop/GeekshubsFSD_Ch1_consoleGame/master/img/mobile.JPG)
+### Upcoming 💭
+* As I progress and get backend knowledge, I will make player2 controllable by another player.
+* I will develope a wrap bigger royale game and this one will be just the battle level.
 
 
 
-
-## Built with 🛠️
-
+### Technologies 🛠️
+No frameworks have been use for this project. Only Vanilla JS.
 * HTML5
 * Javascript ES6
 * CSS3
 
 
 
----
-* Thanks to Geekshubs Academy for the training received (https://github.com/GeeksHubsAcademy)
-
-
-* Created by Javier Monleón López (https://github.com/jmonloop)
-
-
-
-
-Credits of enemy brick textures:
+### Sources Credits
+Player 2 textures:
 <a href='https://pngtree.com/so/pastel'>pastel png from pngtree.com/</a>
 
-Credits of player brick textures:
+Player 1 brick textures:
 <a href='https://pngtree.com/so/brick'>brick png from pngtree.com/</a>
 
-
-Credits of sky textures:
+Sky textures:
 <a href='https://pngtree.com/so/clouds'>clouds png from pngtree.com/</a>
 
-Credits of custom sight:
+Custom pointer:
 <a href='https://pngtree.com/so/red'>red png from pngtree.com/</a>
 
-Credits of main and final theme:
-<a href='https://www.youtube.com/watch?v=LMQ8sSvqphg'>red png from pngtree.com/</a>
+Main and final themes:
+<a href='https://www.youtube.com/watch?v=LMQ8sSvqphg'></a>
 
-Credits of Quake voices:
-<a href='https://www.youtube.com/watch?v=ym4VmVwd24c'>red png from pngtree.com/</a>
+Quake voices:
+<a href='https://www.youtube.com/watch?v=ym4VmVwd24c'></a>
+
+Healing SFX:
+<a href='https://www.youtube.com/watch?v=idVg3eKcbL0'></a>
+
+xVision SFX:
+<a href='https://www.youtube.com/watch?v=TC0KTDMPx5E'></a>
+
+ak47 SFX:
+<a href='https://www.youtube.com/watch?v=1otAua9XIPs'></a>
+<a href='https://www.youtube.com/watch?v=gjbgJNQP6_M'></a>
+
+Mp5 SFX:
+<a href='https://www.youtube.com/watch?v=q7uojrDfEvw'></a>
+
+Desert Eagle SFX:
+<a href='https://www.youtube.com/watch?v=sHffVHCiUto'></a>
+
+AA12 SFX:
+<a href='https://www.youtube.com/watch?v=MYbnuaifdN0'></a>
 
 
 
+### Thanks
+* Thanks to Geekshubs Academy for the training received (https://github.com/GeeksHubsAcademy)
 
+* Thanks to everyone who spends time spreading their knowledge in Stack Overflow.
+
+* Created by Javier Monleón López (https://github.com/jmonloop)
 
 
 
--- js/players.js
+++ js/players.js
@@ -15,6 +15,7 @@ let powerUpToLaunch;
 let powerUpPresent;
 let AIgotPowerUp;
 let AIpowerUp;
+let xVision;
 
 let mainThemeAudio = new Audio("./assets/audio/mainTheme.mp3");
 let endBattleAudio = new Audio("./assets/audio/endBattle.mp3");
@@ -57,11 +58,31 @@ const clickedOutsideWall = () =>{
     return bottomWallClicked;
 }
 
+
+//Character generation:
+let player1Img;
+let player1ImgSrc;
+let player2Img;
+let player2ImgSrc;
+
+let char1ArrayRun = ["./assets/img/01navy/run (1).png","./assets/img/01navy/run (2).png", "./assets/img/01navy/run (3).png", "./assets/img/01navy/run (4).png", "./assets/img/01navy/run (5).png", "./assets/img/01navy/run (6).png", "./assets/img/01navy/run (7).png", "./assets/img/01navy/run (8).png","./assets/img/01navy/run (9).png", "./assets/img/01navy/run (10).png", "./assets/img/01navy/run (11).png", "./assets/img/01navy/run (12).png" ];
+
+let player1AnimationCount = 1;
+
+//Iterates the array of images for make character running animation
+const changeCharAnimation =(char)=>{
+    for(let i=0 ; i<char1ArrayRun.length ; i++){
+        char1ArrayRun[i] = char1ArrayRun[i].replace('01navy', char);
+    };
+};
+
+
+//COORDINATES PRE-FUNCTION
 document.querySelector('#screen3').addEventListener('mousedown', (e)=>{
-// document.querySelector("#_bottomWall").addEventListener('mousedown', (e)=>{
+    //Collects up X position
     xPosition = e.clientX;
     xMinPosition = 1;
-
+    //Collects Y position of character2
     player2Data = document.getElementById('_character2').getBoundingClientRect();
     yTargetPosition = player2Data.top + 50;
     yPosition = e.clientY;
@@ -76,39 +97,13 @@ document.querySelector('#screen3').addEventListener('mousedown', (e)=>{
     if((screen.width > 1436)&&(screen.width <= 1536)) xMaxPosition = screen.width * 0.94;
     if(screen.width <= 1436) xMaxPosition = screen.width * 0.99;
     
-   xPosDifference = xPosition-player1.position;
-   absoluteMovement = Math.abs(xPosition-player1.position);
-
+    //Calculates difference for running animation
+    xPosDifference = xPosition-player1.position;
+    absoluteMovement = Math.abs(xPosition-player1.position);
 
     player1.move();
     return xPosition;
 })
-
-
-
-//GUN CLASS
-class Gun {
-    constructor(name, damage, ammo, initialAmmo, img, shootSound, reloadSound) {
-        this.name = name;
-        this.damage = damage;
-        this.ammo = ammo;
-        this.initialAmmo = initialAmmo;
-        this.img = img;
-        this.shootSound = shootSound;
-        this.reloadSound = reloadSound;
-    }
-    playShoot(){
-        let shootSound = new Audio(this.shootSound);
-        if(!mute)shootSound.play();
-    }
-    playReload(){
-        let reloadSound = new Audio(this.reloadSound);
-        if(!mute)reloadSound.play();
-    }
-}
-
-
-let xVision;
 
 
 //SHOOTING METHOD PRE-FUNCTIONS
@@ -134,34 +129,32 @@ const minMaxRoundedRandom = (min, max) => {
     return Math.round(Math.random() * (max - min) + min);
 }
 
-//Character generation variables declaration:
-let player1Img;
-let player1ImgSrc;
-let player2Img;
-let player2ImgSrc;
 
-let char1ArrayRun = ["./assets/img/01navy/run (1).png","./assets/img/01navy/run (2).png", "./assets/img/01navy/run (3).png", "./assets/img/01navy/run (4).png", "./assets/img/01navy/run (5).png", "./assets/img/01navy/run (6).png", "./assets/img/01navy/run (7).png", "./assets/img/01navy/run (8).png","./assets/img/01navy/run (9).png", "./assets/img/01navy/run (10).png", "./assets/img/01navy/run (11).png", "./assets/img/01navy/run (12).png" ];
-
-let player1AnimationCount = 1;
-
-
-// const replaceCharacter = (char) =>{
-//     for(let x of char1ArrayRun){
-//         x.replace("01navy", char);
-//     }
-//     return char1ArrayRun;
-// }
-
-    
-
-const changeCharAnimation =(char)=>{
-    for(let i=0 ; i<char1ArrayRun.length ; i++){
-        char1ArrayRun[i] = char1ArrayRun[i].replace('01navy', char);
-    };
-};
+//GUN CLASS
+class Gun {
+    constructor(name, damage, ammo, initialAmmo, img, shootSound, reloadSound) {
+        this.name = name;
+        this.damage = damage;
+        this.ammo = ammo;
+        this.initialAmmo = initialAmmo;
+        this.img = img;
+        this.shootSound = shootSound;
+        this.reloadSound = reloadSound;
+    }
+    playShoot(){
+        let shootSound = new Audio(this.shootSound);
+        if(!mute)shootSound.play();
+    }
+    playReload(){
+        let reloadSound = new Audio(this.reloadSound);
+        if(!mute)reloadSound.play();
+    }
+}
 
 
-//Declaration of Character
+
+
+//CHARACTER CLASS
 class Character {
 
     constructor(player, characterType, life, gun, position) {
@@ -176,8 +169,6 @@ class Character {
         //position defines which position is the character when gets out of cover
         this.position = position;
 
-
-
         this.attack = false;
         this.covered = true;
         this.hurt = false;
@@ -186,14 +177,17 @@ class Character {
     show(){
         if(this.player == 1) {
             if(this.covered === true) {
+                //Replace char1 image
                 player1ImgSrc = player1ImgSrc.replace("crouch", "shoot");
                 player1Img.src = player1ImgSrc;
     
                 this.covered = false;
             };
         } else if (this.player == 2) {
+            //Replace char2 style
             document.getElementById('_character2').style.display = "block";
             this.covered = false;
+            //If char2 is out, steals the Power-Up depending on the AIaccuracy level
             if((powerUpPresent)&&(AIaccuracy > 80)){
                 document.querySelector('.powerUpDiv').classList.toggle('getPowerUp');
                 AIgotPowerUp = true;
@@ -217,11 +211,11 @@ class Character {
                 if(!xVision) {
                     document.getElementById('_character2').style.display = "none";
                 } else if(xVision) {
+                    //if player1 has xVision, char2 style when hide changes to crouch
                     player2ImgSrc = player2ImgSrc.replace("shoot", "crouch");
                     player2Img.src = player2ImgSrc;
                 };
                  
-
                 gunPlayer2.ammo = gunPlayer2.initialAmmo;
                 gunPlayer2.playReload();
                 this.covered = true;
@@ -229,29 +223,33 @@ class Character {
         };
     };
 
+    //Moves to another point in X. Must be covered first
     move(){
-
+        //Move limitation depending on break points for avoiding going out of viewport
         if(xPosition > (xMaxPosition)) xPosition = (xMaxPosition);
         if(xPosition < 1) xPosition = 1;
-        
-        //-150 is for centering the image to the click
-        // player2.position = xPosition;
+    
         
         if(this.player == 1) {
             if(bottomWallClicked){
                 if(this.covered==true) {
+                    //Difference btw actual pos and target pos is divided in 8 parts..
                     xPosDifference = xPosDifference / 8;
                     player1ImgSrc = player1ImgSrc.replace("crouch", "run");
                     player1Img.src = player1ImgSrc;
+                    //if it is positive...
                     if(xPosDifference>0){
+                        //and has not arrived yet..
                         if(this.position < this.position + xPosDifference){
+                            //runs an interval iterating the array of running images...
                             let timeId = setInterval(()=>{
+                                //moves div to target position. -150 is for centering the image to the click
                                 document.getElementById("_character1").style.left= (this.position + xPosDifference - 150) +"px";
                                 this.position +=xPosDifference;
                                 
                                 player1Img.src = char1ArrayRun[player1AnimationCount];
                                 player1AnimationCount+=1;
-
+                                //until target position has been reached
                                 if(this.position >= xPosition) {
                                     clearInterval(timeId);
                                     player1AnimationCount = 1;
@@ -281,7 +279,8 @@ class Character {
                         };
                     };
                 };
-            };    
+            };
+            //char2 moves randomly position    
         } else if (this.player == 2) {
             this.position = minMaxRoundedRandom(1,xMaxPosition);
             if(player2.position < 1) player2.position = 1;
@@ -291,6 +290,7 @@ class Character {
     };
 
     shooting = () => {
+        //player1 shoots to x and y position
         if((this.player == 1)&&(!this.covered)&&(gunPlayer1.ammo > 0)) {
             gunPlayer1.ammo -=1;
             this.attack = true;
@@ -306,17 +306,20 @@ class Character {
                 };
             };
 
-            
+            //player2 depends on AI level
         } else if((this.player == 2)&&(!this.covered)&&(gunPlayer2.ammo > 0)){
             gunPlayer2.ammo -=1;
             gunPlayer2.playShoot();
             this.attack = true;
             if((player1.covered== false)||(xVisionAI)) {
+                //AIminAccuracy depends on AI level
                 AIaccuracy = minMaxRoundedRandom(AIminAccuracy,100);
+                //These lines are from increase AIaccuracy if player1 remains static
                 if((absoluteMovement<300)&&(absoluteMovement>200)) AIaccuracy + 5;
                 if((absoluteMovement<200)&&(absoluteMovement>100)) AIaccuracy + 5;
                 if((absoluteMovement<100)&&(absoluteMovement>0)) AIaccuracy + 5;
                 if((absoluteMovement>=300)) AIaccuracy = AIminAccuracy;
+                
                 if(AIaccuracy > 80) {
                     player1.life -= gunPlayer2.damage;
                     player1.hurt = true;
